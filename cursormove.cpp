a12ddb41d09b1af22a804d23c5fc018a89e80e26
Dohinkus <dohinkus@gmail.com>
2021-02-17 23:55:28 -0600

    Add cursor moving functionality

--- cursormove.cpp
+++ cursormove.cpp
@@ -1,20 +1,54 @@
 #include <iostream>
 #include <Windows.h>
 
+//code = Mojave Desert;
+void InputEvent(DWORD type, DWORD flag){
+	INPUT Input = {0};
+	Input.type = type;
+	Input.mi.dwFlags = flag;
+	::SendInput(1, &Input, sizeof(INPUT));
+}
+
+void MouseEvent(DWORD flag){
+	InputEvent(INPUT_MOUSE, flag);
+}
+
+void LeftDown()
+{
+	MouseEvent(MOUSEEVENTF_LEFTDOWN);
+}
+
+void LeftUp()
+{
+	MouseEvent(MOUSEEVENTF_LEFTUP);
+}
+
 int main()
 {
-    HWND window = FindWindow("TaskManagerWindow", "Task Manager");
-    if (window)
-    {
-        RECT rect = {0};
-        GetWindowRect(window, &rect);
 
-        SetForegroundWindow(window);
-        SetActiveWindow(window);
-        SetFocus(window);
-        Sleep(300);
-        SetCursorPos(rect.right - 200, rect.bottom - 200);
-    }
+	HWND window = FindWindow("MSPaintApp", "Untitled - Paint");
+	if (window)
+	{
+		RECT rect = {0};
+		GetWindowRect(window, &rect);
 
-    return 0;
+		SetForegroundWindow(window);
+		SetActiveWindow(window);
+		SetFocus(window);
+		
+		SetCursorPos(rect.left + 50 , rect.bottom - 200 );
+		LeftDown();
+		for (int x = 0; x < 700; x+=5)
+		{
+
+			for (int y = 0; y < 300; y+=150)
+			{
+				Sleep(1);
+				SetCursorPos(rect.left + 50 + x, rect.bottom - 200 - y);
+			}
+		}
+		LeftUp();
+	}
+
+	return 0;
 }
