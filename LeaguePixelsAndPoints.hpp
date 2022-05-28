d17573f92ce52e691155b7a3df9e771e1b85628b
Dohinkus <39741743+Dohinkus@users.noreply.github.com>
2022-05-28 05:37:02 -0500

    Split data into two header files
    The future league bots will include the basic pixels all bots use, and then have their own more specific pixels in a separate header for organization

---
+++ LeaguePixelsAndPoints.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "Pixel.hpp"
+
+//Pixels
+constexpr Pixel IN_GAME_HUD{ {1641, 1030}, 129, 106, 72 };
+constexpr Pixel SHOP_BRIGHT{ {1141, 1070}, 177, 157, 85 };
+constexpr Pixel EMPTY_ITEM_SLOT_1{ {1130, 946}, 0, 0, 0 };
+constexpr Pixel EMPTY_ITEM_SLOT_2{ {1179, 946}, 0, 0, 0 };
+constexpr Pixel EMPTY_ITEM_SLOT_3{ {1228, 946}, 0, 0, 0 };
+constexpr Pixel EMPTY_ITEM_SLOT_4{ {1130, 993}, 0, 0, 0 };
+constexpr Pixel EMPTY_ITEM_SLOT_5{ {1179, 993}, 0, 0, 0 };
+constexpr Pixel EMPTY_ITEM_SLOT_6{ {1228, 993}, 0, 0, 0 };
+constexpr Pixel CAN_LEVEL_UP_ABILITY{ {794, 876}, 255, 247, 153 };
+constexpr Pixel FULL_HEALTH{ {1094, 1030}, 51, 210, 58 };
+constexpr Pixel ZERO_HEALTH{ {682, 1030}, 16, 89, 24 };
+constexpr Pixel FULL_MANA{ {1094, 1064}, 88, 171, 250 };
+constexpr Pixel ZERO_MANA{ {682, 1064}, 0, 70, 150 };
+constexpr Pixel Q_USABLE{ {730, 974}, 198, 162, 99 };
+constexpr Pixel W_USABLE{ {797, 973}, 198, 162, 99 };
+constexpr Pixel E_USABLE{ {} };
+constexpr Pixel R_USABLE{ {} };
+//POINTs
+constexpr POINT ALLY_BASE{ 1668, 1053 };
--- LevelingBotPixelsAndPoints.hpp
+++ LevelingBotPixelsAndPoints.hpp
@@ -1,37 +1,21 @@
 #pragma once
-#include "Pixel.hpp"
+#include "LeaguePixelsAndPoints.hpp"
 
 //Pixels
-constexpr Pixel IN_GAME_HUD{ {1641, 1030}, 129, 106, 72 };
 constexpr Pixel CLIENT_BORDER{ {320, 160}, 30, 40, 45 };
-constexpr Pixel SHOP_BRIGHT{ {1141, 1070}, 177, 157, 85 };
 constexpr Pixel FOW_ALLY_MID_TOWER_1{ {1762, 958}, 52, 55, 40 };
 constexpr Pixel FOW_ALLY_MID_TOWER_2{ {1739, 980}, 52, 55, 40 };
 constexpr Pixel FOW_ALLY_MID_TOWER_3{ {1729, 991}, 52, 55, 40 };
-constexpr Pixel EMPTY_ITEM_SLOT_1{ {1130, 946}, 0, 0, 0 };
-constexpr Pixel EMPTY_ITEM_SLOT_2{ {1179, 946}, 0, 0, 0 };
-constexpr Pixel EMPTY_ITEM_SLOT_3{ {1228, 946}, 0, 0, 0 };
-constexpr Pixel EMPTY_ITEM_SLOT_4{ {1130, 993}, 0, 0, 0 };
-constexpr Pixel EMPTY_ITEM_SLOT_5{ {1179, 993}, 0, 0, 0 };
-constexpr Pixel EMPTY_ITEM_SLOT_6{ {1228, 993}, 0, 0, 0 };
 constexpr Pixel DORANS_SHIELD_ITEM_SLOT_1{ {1152, 954}, 106, 66, 36 };
 constexpr Pixel FOW_MID_TOWER_1_A{ {1813, 907}, 52, 55, 40 };
 constexpr Pixel FOW_MID_TOWER_1_B{ {1828, 901}, 52, 55, 40 };
 constexpr Pixel FOW_MID_TOWER_2{ {1854, 880}, 36, 49, 52 };
 constexpr Pixel FOW_MID_TOWER_3{ {1869, 854}, 41, 54, 57 };
 constexpr Pixel FOW_NEXUS_TOWERS{ {1902, 834},	 41, 54, 56 };
-constexpr Pixel CAN_LEVEL_UP_ABILITY{ {794, 876}, 255, 247, 153};
-constexpr Pixel FULL_HEALTH{ {1094, 1030}, 51, 210, 58 };
-constexpr Pixel ZERO_HEALTH{ {682, 1030}, 16, 89, 24 }; 
-constexpr Pixel FULL_MANA{ {1094, 1064}, 88, 171, 250 };
-constexpr Pixel ZERO_MANA{ {682, 1064}, 0, 70, 150 };
-constexpr Pixel Q_USABLE{ {730, 974}, 198, 162, 99 };
-constexpr Pixel W_USABLE{ {797, 973}, 198, 162, 99 };
 //POINTs
 constexpr POINT SAFE_RECALL_MID_TOWER_1{1748, 973};
 constexpr POINT SAFE_RECALL_MID_TOWER_2{1725, 997};
 constexpr POINT SAFE_RECALL_MID_TOWER_3{1709, 1022};
-constexpr POINT ALLY_BASE{1668, 1053};
 constexpr POINT SAFE_ATTACK_NEXUS{1876, 845};
 constexpr POINT SAFE_ATTACK_MID_TOWER_1_A{1777, 948};
 constexpr POINT SAFE_ATTACK_MID_TOWER_1_B{1790, 936};
