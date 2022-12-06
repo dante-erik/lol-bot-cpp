3c0e73a085b08b0f330297904315600827f2d709
Dohinkus <39741743+Dohinkus@users.noreply.github.com>
2022-12-06 15:50:43 -0600

    Add core

---
+++ LeaguePixels.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Pixel.hpp"
+
+//if only an {x, y} point is given without an RGB, the pixel is never used for comparisons
+
+constexpr Pixel GAME_HUD{ {1331, 1060}, 138, 121, 74 };
+//green lit pixel on healthbar around 65% HP,
+//the LOW_HEALTH pixel became changed after 1 tower shot on ahri at lvl 3 at 7ish minutes
+constexpr Pixel LOW_HEALTH{ {980, 1030}, 11, 171, 11 };
+//actually only ~92% HP but the remaining 8% is regained while walking out of base
+constexpr Pixel FULL_HEALTH{ {1078, 1030}, 9, 194, 1 };
+constexpr Pixel ALLY_BASE{ {1666, 1055} };
+constexpr Pixel ENEMY_NEXUS{ {1882, 844} };
+constexpr Pixel FIND_MATCH{ {847, 831}, 20, 62, 85 };
+constexpr Pixel FIND_MATCH_HIGHLIGHTED{ {847, 831}, 18, 89, 110 };
+constexpr Pixel ACCEPT_MATCH{ {961, 706}, 30, 37, 42 };
+constexpr Pixel ACCEPT_MATCH_HIGHLIGHTED{ {961, 706}, 26, 46, 56 };
+constexpr Pixel CHAMPION_SELECT{ {588, 198}, 67, 52, 19 };
+constexpr Pixel HONOR_TEAMMATE{ {958, 805}, 68, 72, 66 };
+constexpr Pixel PLAY_AGAIN{ {846, 830}, 20, 69, 92 };
+constexpr Pixel PLAY_AGAIN_HIGHLIGHTED{ {846, 830}, 18, 93, 114 };
+constexpr Pixel CHAMPION_REWARD{ {961, 835 }, 57, 59, 60};
+constexpr Pixel CHAMPION_LOCKED_IN{ {1156, 838}, 205, 190, 145 };
+constexpr Pixel TOP_LEFT_CHAMPION_ICON{ {706, 326} };
+constexpr Pixel LOCK_IN{ {959, 766} };
+//selects middle of the three champion options
+constexpr Pixel CHAMPION_REWARD_SELECT_BUTTON{ {943, 547} };
+constexpr Pixel CHAMPION_REWARD_OKAY_BUTTON{ {959, 831} };
+//this encompasses rewards from leveling up, daily bonus, missions, and quests
+constexpr Pixel GENERIC_REWARD{ {961, 835}, 139, 132, 107 };
+constexpr Pixel GENERIC_REWARD_HIGHLIGHTED{ {961, 835}, 141, 134, 107 };
+constexpr Pixel PROBLEM_SELECTING_YOUR_CHAMPION{ {961, 549}, 157, 141, 102 };
+constexpr Pixel PROBLEM_SELECTING_YOUR_CHAMPION_HIGHLIGHTED{ {961, 549}, 206, 192, 167 };
---
+++ lol_leveling_bot.cpp
@@ -0,0 +1,136 @@
+#include "Robot.hpp"
+#include "LeaguePixels.hpp"
+#include <memory>
+
+#define VISIBLE(pixelName) robot->isPixelEqual(pixelName)
+#define LEFT_CLICK(pointName) robot->leftClick(pointName.p, clickDuration)
+#define RIGHT_CLICK(pointName) robot->rightClick(pointName.p, clickDuration)
+
+//simulates all the input events as if a human were inputting mouse / keyboard events
+std::unique_ptr<Robot> robot = std::make_unique<Robot>();
+
+//milliseconds for click events using mouse and keyboard
+constexpr int clickDuration = 50;
+
+void championSelectActions() {
+	//loop until champion is locked in & still in champion select
+	while (!VISIBLE(CHAMPION_LOCKED_IN) && VISIBLE(CHAMPION_SELECT)) {
+
+		LEFT_CLICK(TOP_LEFT_CHAMPION_ICON);
+		LEFT_CLICK(LOCK_IN);
+
+		robot->updateScreenBuffer();
+	}
+}
+
+void acceptChampionReward() {
+	LEFT_CLICK(CHAMPION_REWARD_SELECT_BUTTON);
+	//client lags after selecting the champion
+	Sleep(4000);
+	LEFT_CLICK(CHAMPION_REWARD_OKAY_BUTTON);
+}
+
+void levelUpAbilities() {
+	robot->ctrlPlusKeyClick("rqwe", clickDuration);
+}
+
+void buyItem(const char* itemName) {
+	//open shop
+	robot->keyClick('p', clickDuration);
+
+	//shop search bar hotkey
+	robot->ctrlPlusKeyClick('l', clickDuration);
+
+	//search for item
+	robot->keyClick(itemName, clickDuration);
+
+	//buy item
+	robot->enterKeyClick(clickDuration);
+
+	//close shop
+	robot->escapeKeyClick(clickDuration);
+}
+
+void gameActions(bool& isNewGame) {
+	if (isNewGame) {
+		levelUpAbilities();
+		//buys steel shoulderguards support item for easier minion killing / faster pushing
+		buyItem("shoulderguards");
+		//wait for minions to spawn
+		Sleep(60000);
+
+		isNewGame = false;
+	}
+	levelUpAbilities();
+	//health > 30%, attack enemy base
+	if (VISIBLE(LOW_HEALTH)) {
+		robot->keyClick('a', clickDuration);
+		LEFT_CLICK(ENEMY_NEXUS);
+	}
+	//health < 30%, walk to ally base and wait until full health is reached
+	else {
+		while (!VISIBLE(FULL_HEALTH) && VISIBLE(GAME_HUD)) {
+			RIGHT_CLICK(ALLY_BASE);
+			robot->updateScreenBuffer();
+		}
+	}
+}
+
+void clientActions(bool& isNewGame) {
+	//client bug: find match randomly highlights after games regardless of cursor position
+	//fix: add highlighted pixel value for find match
+	if (VISIBLE(FIND_MATCH) || VISIBLE(FIND_MATCH_HIGHLIGHTED)) {
+		isNewGame = true;
+		LEFT_CLICK(FIND_MATCH);
+	}
+	else if (VISIBLE(ACCEPT_MATCH) || VISIBLE(ACCEPT_MATCH_HIGHLIGHTED)) {
+		LEFT_CLICK(ACCEPT_MATCH);
+	}
+	else if (VISIBLE(CHAMPION_SELECT)) {
+		championSelectActions();
+	}
+	else if (VISIBLE(HONOR_TEAMMATE)) {
+		//client lags when displaying honor teammate
+		Sleep(4000);
+		LEFT_CLICK(HONOR_TEAMMATE);
+	}
+	else if (VISIBLE(PLAY_AGAIN) || VISIBLE(PLAY_AGAIN_HIGHLIGHTED)) {
+		LEFT_CLICK(PLAY_AGAIN);
+	}
+	else if (VISIBLE(GENERIC_REWARD) || VISIBLE(GENERIC_REWARD_HIGHLIGHTED)) {
+		//client lags when displaying rewards
+		Sleep(4000);
+		LEFT_CLICK(GENERIC_REWARD);
+	}
+	else if (VISIBLE(CHAMPION_REWARD)) {
+		//client lags when displaying champion reward
+		Sleep(4000);
+		acceptChampionReward();
+	}
+	else if (VISIBLE(PROBLEM_SELECTING_YOUR_CHAMPION) || VISIBLE(PROBLEM_SELECTING_YOUR_CHAMPION_HIGHLIGHTED)) {
+		LEFT_CLICK(PROBLEM_SELECTING_YOUR_CHAMPION);
+	}
+}
+
+
+
+int main()
+{
+	bool isNewGame = false;
+
+	while (true) {
+		robot->updateScreenBuffer();
+
+		//in game
+		if (VISIBLE(GAME_HUD)) {
+			gameActions(isNewGame);
+		}
+		//in client or nothing visible
+		else {
+			clientActions(isNewGame);
+		}
+	}
+
+	return 0;
+}
+
